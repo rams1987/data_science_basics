e72442abd1b47f080e0160344ab44f8757aa5165
rams1987 <ramasamy.seenivasagan@gmail.com>
2017-12-20 13:30:38 +0530

    testing

--- data_science_tutorial_basics.py
+++ data_science_tutorial_basics.py
@@ -32,6 +32,7 @@ y = labelencoder_y.fit_transform(y)
 from sklearn.cross_validation import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+print(X_train)
 
 
 
