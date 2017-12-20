28e986d55c3cb706a07ed63d3903d40c373a16ae
rams1987 <ramasamy.seenivasagan@gmail.com>
2017-12-20 17:14:44 +0530

    Added - feature scaling

--- data_science_tutorial_basics.py
+++ data_science_tutorial_basics.py
@@ -35,6 +35,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 print(X_train)
 
 
-
-
-
+#Feature scaling - because there will a lot of deviation in feature vectors which makes ML algorithms to run a long time.
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
